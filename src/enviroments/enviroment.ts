e103f5611ddf6ef3a6ade272e7099c738c85de7d
MohamadAlaskari <m_alskari96@hotmail.com>
2024-07-09 17:33:54 +0200

    fix port

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:3000',
+  apiBaseUrl: 'http://localhost:3001',
   endpoints: {
     employee: {
       getAll: '/employee/employees',
